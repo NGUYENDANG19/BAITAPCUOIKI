c0099e355a54cbe1ea3534f1ab2dcf3b48577523
NGUYENDANG19 <113655612+NGUYENDANG19@users.noreply.github.com>
2022-12-15 20:53:27 +0700

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -4,117 +4,131 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.format("nhập vào n :");
-		int n = sc.nextInt();
-		
-		chanLe(n);
-		tong(n);
-		chiaHetCho5(n);
-		tongS(n);
-		tinhGiaiThua(n);
-		thangTrongNam(n);
-	}
+		String name;
+	    int year;
+	    int old;
+	    Scanner sc = new Scanner(System.in);
+	    System.out.format("Nhập vào họ tên của sinh viên: ");
+	    name = sc.nextLine();
+	    System.out.format("Nhập vào năm sinh của sinh viên: ");
+	    year = sc.nextInt();
+	    old = 2022 - year;
+	    
+	    System.out.println("Họ tên của sinh viên: " + name);
+	    System.out.println("Tuổi của sinh viên :  " + old);
 
-	private static void tongS(int n) {
-		// TODO Auto-generated method stub
-		 float sum = 0;
-	      Scanner sc;
-	        do {
-	            System.out.format("nhập n = ");
-	            sc = new Scanner(System.in);
-	            n = sc.nextInt();
-	        }while(n <= 0);
-	         
-	        for(int i = 1; i <= n; i++) {
-	            sum += 1.0/i;
-	        }
-	         
-	        System.out.println(" - Sum = " + sum);
-	        sc.close();	
-	}
 
-	private static void thangTrongNam(int n) {
-		// TODO Auto-generated method stub
-		System.out.println();
-		switch (n)	{
-		case   1:
-			System.out.format("đây là tháng một ");
-			break;
-		case   2:
-			System.out.format("đây là tháng hai ");
-			break;
-		case   3:
-			System.out.format("đây là tháng ba ");
-			break;
-		case   4:
-			System.out.format("đây là tháng bốn ");
-			break;
-		case   5:
-			System.out.format("đây là tháng năm ");
-			break;
-		case   6:
-			System.out.format("đây là tháng sáu ");
-			break;
-		case   7:
-			System.out.format("đây là tháng bảy ");
-			break;
-		case   8:
-			System.out.format("đây là tháng tám ");
-			break;
-		case   9:
-			System.out.format("đây là tháng chín ");
-			break;
-		case   10:
-			System.out.format("đây là tháng mười ");
-			break;
-		case   11:
-			System.out.format("đây là tháng mười một ");
-			break;
-		case   12:
-			System.out.format("đây là tháng mười hai ");
-			
-			break;
-		default:
-			System.out.println("sai du lieu");
-		}
-	}
+	    float diemToan, diemLy, diemVan;
+	    System.out.format("Nhập vào điểm toán : ");
+	    diemToan = sc.nextFloat();
+	    System.out.format("Nhập vào điểm lý: ");
+	    diemLy = sc.nextFloat();
+	    System.out.format("Nhập vào điểm văn: ");
+	    diemVan = sc.nextFloat();
+	    cau2 (diemToan,diemLy,diemVan);
 
-	private static void tinhGiaiThua(int n) {
-		// TODO Auto-generated method stub
-		 int ketQua = 1;
-		 for(int i=1;i<n;i++) {
-				ketQua = ketQua * i;		
-		}
-		System.out.println(" tong gia thua la = "+ketQua);
-	}
+	    int A,B;
+	    System.out.format("Nhập vào cạnh A : ");
+	    A = sc.nextInt();
+	    System.out.format("Nhập vào cạnh B: ");
+	    B = sc.nextInt();
 
-	private static void chiaHetCho5(int n) {   
-		System.out.println();
-		// TODO Auto-generated method stub
-		if(n%5==0) {
-			System.out.println("- n chia hết  cho 5");
-		}else {
-			System.out.println("- n không chia hết cho 5");
-		}
-	}
+	    cau3(A,B);
 
-	private static void tong(int n) {
-		// TODO Auto-generated method stub
-		System.out.println();
-		int tong = 0;
-		for (int i = 1;i<=n;i++ ) {
-			tong = tong + i;
-		}
-		System.out.println(" - tong = "+tong);
-	}
+	   float x,y;
+	    System.out.format("Nhập vào cạnh x : ");
+	    x = sc.nextFloat();
+	    System.out.format("Nhập vào cạnh y: ");
+	    y = sc.nextFloat();
 
-	private static void chanLe(int n) {
-		// TODO Auto-generated method stub
-		if (n%2==0) {
-			System.out.format("\n - n là số chẵn");
-		}else {
-			System.out.format("\n -  n là số lẽ");
-		}
+	    cau4 (x,y);
+
+	    float a,b,c;
+	    System.out.format("Nhập vào  a : ");
+	    a = sc.nextFloat();
+	    System.out.format("Nhập vào b: ");
+	    b = sc.nextFloat();
+	    System.out.format("Nhập vào c: ");
+	    c = sc.nextFloat();
+
+	    cau5 (a,b,c);
+
+	    int m,n;
+	    System.out.format("Nhập vào  m : ");
+	    m = sc.nextInt();
+	    System.out.format("Nhập vào n: ");
+	    n = sc.nextInt();
+
+	    cau6(m,n);
+	  }
+
+	  private static void cau2(float diemToan,float diemLy,float diemVan) {
+	    // TODO Auto-generated method stub
+		double diemTrungBinh;
+	    diemTrungBinh = (diemToan + diemLy + diemVan)  /3;
+
+
+	    System.out.println(" điểm trung bình"+diemTrungBinh);
+
+	  } 
+	    private static void cau3(int A,int B) {
+	        // TODO Auto-generated method stub
+	    float dienTich;
+	    float chuVi;
+
+	    dienTich = (A*B)	;
+	    chuVi = (A+B)*2	;
+
+	    System.out.println("Diện tích : " + dienTich);
+	    System.out.println("Chu Vi" + chuVi);
+	    } 
+	    private static void cau4(float x,float y) {
+	        // TODO Auto-generated method stub
+	    	float tong,hieu,tich,thuong;
+
+	    tong = x+y;
+	    hieu = x-y;
+	    tich = x*y;
+	    thuong = x/y;
+
+	    System.out.println("Tổng : " + tong);
+	    System.out.println("Hiệu" + hieu);
+	    System.out.println("Tích : " + tich);
+	    System.out.println("Thương" + thuong);
+
+	    }
+
+	    private static void cau5(float a,float b,float c) {
+	      // TODO Auto-generated method stub
+	    float min,max; 
+	    max = min = a;
+
+	    if(b > max){
+	     max = b;
+	     }
+
+	    if(c > max){
+	     max = c;
+	     }
+	    System.out.println("\n số lớn nhất : "+ max);
+
+	    if(b < min){
+	     min = b;
+	     }
+
+	     if(c < min){
+	      min = c;
+	     }
+	    System.out.println("\n số be nhất : "+ min);
+
+	    } 
+	    private static void cau6(int m,int n) {
+	        // TODO Auto-generated method stub
+
+	    	if (m % n ==0) {
+	    		System.out.println("m chia hết cho n");
+	    	}else	{
+	    		System.out.println("m không chia hết cho n");
+	    	}
+	  }
 	}
-}
